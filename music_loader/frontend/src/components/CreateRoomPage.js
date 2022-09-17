d419316557f69807a5af7a4682eea7be99a30d92
Phython Phil <prshents@hotmail.co.uk>
2022-09-17 21:12:41 +0100

    mui/materials added to frontend

--- music_loader/frontend/src/components/CreateRoomPage.js
+++ music_loader/frontend/src/components/CreateRoomPage.js
@@ -1,11 +1,23 @@
 import React, {Component} from "react";
+import { Button, Grid, Typography, TextField, FormHelperText, FormControl, 
+    FormControlLabel, RadioGroup, Radio } from "@mui/material";
 
 export default class CreateRoomPage extends Component {
+    defaultVotes = 2;
+    
     constructor(props){
         super(props);
     }
 
     render() {
-        return <p>This is the Create room page</p>
+        return (
+            <Grid contaniner spacing={1}>
+                <Grid item xs={12} align="center">
+                    <Typography component="h4" variant="h4">
+                        Create a room
+                    </Typography>
+                </Grid>
+            </Grid>
+        );
     }
 }
